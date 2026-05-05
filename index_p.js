a0f51b8de86e9e6a7829ec1fbc0d65b43b95ea1a
ANKITA <ankita.mca2007@gmail.com>
2026-05-05 10:10:16 +0530

    changes

--- index_p.js
+++
@@ -1,5 +0,0 @@
-// 1. Program to find longest word in a given sentence ? ['a','abcd','abc','ab'] // o/p : abcd
-// 2. How to check whether a string is palindrome or not ?
-// 3. Write a program to remove duplicates from an array ?
-// 4. Program to find Reverse of a string without using built-in method ?
-// 5. Find the max count of consecutive 1’s in an array ? [1,0,1,1,1,1,5,7,8,11,3,1,1,1,7,6,5,1,1,0,1,0] //op : 4
